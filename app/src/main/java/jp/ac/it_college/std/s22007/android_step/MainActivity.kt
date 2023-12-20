1bf37884ae3e199cd95144ec5561ab8a3a623ca0
s22007 <s22007@it-college.ac.jp>
2023-12-20 12:28:49 +0900

    一応ホーム画面のやつはだいたい

--- app/src/main/java/jp/ac/it_college/std/s22007/android_step/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s22007/android_step/MainActivity.kt
@@ -1,24 +1,51 @@
 package jp.ac.it_college.std.s22007.android_step
-
+import android.content.Context
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import jp.ac.it_college.std.s22007.android_step.ui.theme.Android_StepTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
             Android_StepTheme {
                 Navigation()
             }
         }
     }
 }
+
+
+
+
+//@Composable
+//fun StepCounterDisplays(sensorManager: SensorManager) {
+//    var stepCount by remember { mutableStateOf(0) }
+//
+//    DisposableEffect(sensorManager) {
+//        val sensorEventListener = object : SensorEventListener {
+//            override fun onSensorChanged(event: SensorEvent?) {
+//                event?.let {
+//                    if (it.sensor.type == Sensor.TYPE_STEP_COUNTER) {
+//                        val steps = it.values[0].toInt()
+//                        stepCount = steps
+//                    }
+//                }
+//            }
+//
+//            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+//        }
+//
+//        val stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+//        sensorManager.registerListener(sensorEventListener, stepCounterSensor, SensorManager.SENSOR_DELAY_NORMAL)
+//
+//        onDispose {
+//            sensorManager.unregisterListener(sensorEventListener)
+//        }
+//    }
+//    Text(text = "Steps: $stepCount")
+//}
