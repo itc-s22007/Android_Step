b375b4947d2e197ffae1c27d59aa034ff364cb25
s22024 <s22024@std.it-college.ac.jp>
2023-12-22 11:32:14 +0900

    homdのエラー解消

--- app/src/main/java/jp/ac/it_college/std/s22007/android_step/home/HomeScene.kt
+++ app/src/main/java/jp/ac/it_college/std/s22007/android_step/home/HomeScene.kt
@@ -6,12 +6,7 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import android.media.Image
-import android.util.Log
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -19,46 +14,24 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -67,11 +40,9 @@ import androidx.core.content.ContextCompat.startActivity
 import jp.ac.it_college.std.s22007.android_step.R
 import jp.ac.it_college.std.s22007.android_step.sns.shereIntent
 import jp.ac.it_college.std.s22007.android_step.ui.theme.Android_StepTheme
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import java.time.LocalDate
 import java.time.LocalTime
-
 
 
 @Composable
@@ -84,7 +55,6 @@ fun HomeScene(
     var currentData by remember { mutableStateOf("") }
     val currentLocalTime = remember { mutableStateOf(LocalTime.now()) }
     val goalSteps by remember { mutableStateOf(5000) }
-    val goalSteps = remember { mutableStateOf(5000) }
     val context = LocalContext.current
     LaunchedEffect(Unit) {
         while (true) {
@@ -210,24 +180,28 @@ fun HomeScene(
 
 
             Row {
-                Button(onClick = onClickTimerButton, modifier = Modifier.padding(5.dp)
+                Button(
+                    onClick = onClickTimerButton, modifier = Modifier.padding(5.dp)
                 ) {
                     Text(text = stringResource(id = R.string.to_timer))
                 }
-                Button(onClick = onClickMapButton, modifier = Modifier.padding(5.dp)
-                ){
+                Button(
+                    onClick = onClickMapButton, modifier = Modifier.padding(5.dp)
+                ) {
                     Text(text = stringResource(id = R.string.to_Map))
                 }
-                Button(onClick = {
-                    startActivity(context, shereIntent, null)
-                }, modifier = Modifier.padding(5.dp)
-                ){
+                Button(
+                    onClick = {
+                        startActivity(context, shereIntent, null)
+                    }, modifier = Modifier.padding(5.dp)
+                ) {
                     Text("share")
                 }
             }
         }
     }
 }
+
 @Composable
 fun StepCounterDisplays(steps: MutableState<Int>) {
     val sensorManager =
@@ -260,11 +234,11 @@ fun StepCounterDisplays(steps: MutableState<Int>) {
 }
 
 
-
 fun isButtonEnabled(currentDate: LocalDate): Boolean {
     val sevenDaysAgo = LocalDate.now().minusDays(7)
     return currentDate > sevenDaysAgo
 }
+
 fun fetchDataForDate(date: LocalDate): String {
     return "データ: $date"
 }
